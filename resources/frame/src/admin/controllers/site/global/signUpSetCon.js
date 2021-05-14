b59740b2e81fd9ebfa02aed05a11707fa44a33ca
marcoyu <marcoyu@tencentcom>
2021-05-14 10:40:47 +0800

    feature:删除注册与登录模式设置

--- resources/frame/src/admin/controllers/site/global/signUpSetCon.js
+++ resources/frame/src/admin/controllers/site/global/signUpSetCon.js
@@ -12,10 +12,6 @@ export default {
       checkList:[],           //密码规则
       register_captcha:'',    //验证码开始
       disabled:true,            //是否可以开启验证码
-      register_type: 0,      // 注册模式
-      qcloud_name: false,
-      qcloud_sms: false,
-      qcloud_wx: false,
       privacy: "0", //隐私协议
       register: "0", //用户协议
       register_content:'',
@@ -51,34 +47,11 @@ export default {
           this.pwdLength = res.readdata._data.set_reg.password_length;
           this.checkList = res.readdata._data.set_reg.password_strength;
           this.register_captcha = res.readdata._data.set_reg.register_captcha;
-          this.register_type = res.readdata._data.set_reg.register_type;
           this.privacy = agreement.privacy ? "1" : "0";
           this.register = agreement.register ? "1" : "0";
           this.register_content = agreement.register_content;
           this.privacy_content = agreement.privacy_content;
           this.extensionOn = res.readdata._data.set_site.open_ext_fields === '1' ? true : false;
-          // 旧注册登陆模式的禁用控制
-          if(res.readdata._data.qcloud.qcloud_sms == true) {
-            this.qcloud_sms = false
-          } else {
-            this.qcloud_sms = true
-          }
-          // 第三方登录设置：公众号配置、小程序配置、PC端微信扫码登录全部开启，才可选无感模式
-          if(
-            res.readdata._data.passport.offiaccount_close
-              && res.readdata._data.passport.miniprogram_close
-              && res.readdata._data.passport.oplatform_close
-          ) {
-            this.qcloud_wx = false
-          } else {
-            this.qcloud_wx = true;
-          }
-
-          // 新注册登陆模式的禁用控制
-          // this.qcloud_name = !res.readdata._data.sign_enable.user_name;
-          // this.qcloud_sms = !res.readdata._data.sign_enable.mobile_phone;
-          // this.qcloud_wx = !res.readdata._data.sign_enable.wechat_direct;
-
 
           if(res.readdata._data.qcloud.qcloud_captcha == true){
             this.disabled = false
@@ -233,13 +206,6 @@ export default {
               "attributes":{
                 "key":'password_strength',
                 "value":passwordStrength,
-                "tag": 'default'
-               }
-            },
-            {
-              "attributes":{
-                "key":'register_type',
-                "value":this.register_type,
                 "tag": 'default'
                }
             },
