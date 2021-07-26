21f7203534e111094096b041c78f9a80fc694b3f
lijunjun <li13754950174@163.com>
2021-07-26 16:35:07 +0800

    feat: 页面统计规则添加

--- resources/frame/src/admin/view/site/global/siteSet/siteDataRulesView.vue
+++ resources/frame/src/admin/view/site/global/siteSet/siteDataRulesView.vue
@@ -9,12 +9,12 @@
       <Card class="sort-switch-radio">
         <p class="sort-switch-radio_title">阅读数计算方式</p>
         <div class="sort-switch-radio_option">
-          <el-radio v-model="radio" label="1" :class="radio === '1' ? 'sort-switch-radio_cont' : ''">仅点金帖子详情页增加阅读数</el-radio>
+          <el-radio v-model="radio" label="1" :class="radio === '1' ? 'sort-switch-radio_cont' : ''">仅点进帖子详情页增加阅读数</el-radio>
         </div>
         <div>
           <el-radio v-model="radio" label="0" :class="radio === '0' ? 'sort-switch-radio_cont' : ''">操作首页帖子、进入详情页，增加阅读数</el-radio>
         </div>
-        <p class="sort-switch-radio_explain">说明：操作报错点赞、点击"查看更多、分享、下载附件、点开图片预览、播放视频、播放语音、点击帖子中包含的链接或者话题。</p>
+        <p class="sort-switch-radio_explain">说明：操作包括点赞、点击"查看更多"、分享、下载附件、点开图片预览、播放视频、播放语音、点击帖子中包含的链接或话题。</p>
       </Card>
       <el-button type="primary" size="medium" @click="ruleSubmission">提交</el-button>
     </div>
