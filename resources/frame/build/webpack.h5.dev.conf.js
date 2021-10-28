0961d5590dfed1780233cda534c856de0f82fead
lijunjun <li13754950174@163.com>
2021-10-28 18:31:53 +0800

    feat: 小商品插件

--- resources/frame/build/webpack.h5.dev.conf.js
+++ resources/frame/build/webpack.h5.dev.conf.js
@@ -148,7 +148,7 @@ module.exports = {
         // target: "https://discuz.run",
         // target: "https://dq.comsenz-service.com/",
         // target: "http://dev.discuz.com/",
-        target: "http://txdev.wentguo.com/",
+        target: "https://bbsv3.techo.chat/",
         changeOrigin: true,
         secure: false,
       }
