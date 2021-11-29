42c335cc3dfc94d9c4a3b43ccdf1f22c57fd9f05
lijunjun <li13754950174@163.com>
2021-11-29 15:00:25 +0800

    feat: 二期插件

--- resources/frame/src/admin/controllers/site/global/pluginUnitCon.js
+++ resources/frame/src/admin/controllers/site/global/pluginUnitCon.js
@@ -6,24 +6,276 @@ export default {
   data:function () {
     return {
       tableData: [],
-      dialogVisible: false,
+      dialogVisible: true,
       fileList: [],
       uploadType: '',
       uploadDetails: '',
+      radio: '',
+      checkList: ['选中且禁用','复选框 A'],
+      settingData: [
+        {
+          "field_key": "appid", // 字段key值，提交的时候需要
+          "field_name": "appid", // 字段名称
+          "field_ext": {}, // 字段扩展信息，主要是单选、多选、下拉框类型的字段会用到。其它字段类型暂时用不到
+          "field_placeholder": "请输入微信小商店 Appid", // 字段占位符（主要是文本框输入之前的提示信息）
+          "field_tip": "", // 字段提示
+          "type": 0, // 字段类型：0 - 单行文本框；1 - 多行文本框；2 - 单选；3 - 多选；4 - 图片上传；5 - 附件上传；6 - 下拉框；7 - 可添加的单行文本框，8 - 开关，9 - 按钮
+          "type_desc": "单行文本框" // 字段类型描述
+        },
+        {
+          "field_key": "secret_key",
+          "field_name": "secret key",
+          "field_ext": {}, // 字段扩展信息
+          "field_placeholder": "请输入微信小商店 secret key",
+          "field_tip": "",
+          "type": 0,
+          "type_desc": "单行文本框"
+        },
+        {
+          "field_key": "desc",
+          "field_name": "小店介绍",
+          "field_ext": {}, // 字段扩展信息
+          "field_placeholder": "请输入微信小商店介绍",
+          "field_tip": "",
+          "type": 1,
+          "type_desc": "多行文本框"
+        },
+        {
+          "field_key": "desc",
+          "field_name": "小店介绍",
+          "field_ext": {}, // 字段扩展信息
+          "field_placeholder": "请输入微信小商店介绍",
+          "field_tip": "",
+          "type": 2,
+          "type_desc": "多行文本框"
+        },
+        {
+          "field_key": "desc",
+          "field_name": "小店介绍",
+          "field_ext": {}, // 字段扩展信息
+          "field_placeholder": "请输入微信小商店介绍",
+          "field_tip": "",
+          "type": 3,
+          "type_desc": "多行文本框"
+        },
+        {
+          "field_key": "desc",
+          "field_name": "小店介绍",
+          "field_ext": {}, // 字段扩展信息
+          "field_placeholder": "请输入微信小商店介绍",
+          "field_tip": "",
+          "type": 4,
+          "type_desc": "多行文本框"
+        },
+        {
+          "field_key": "desc",
+          "field_name": "小店介绍",
+          "field_ext": {}, // 字段扩展信息
+          "field_placeholder": "请输入微信小商店介绍",
+          "field_tip": "",
+          "type": 5,
+          "type_desc": "多行文本框"
+        },
+        // 这里是一个下拉框的例子
+        {
+          "field_key": "select",
+          "field_name": "内容导入",
+          "field_ext": {
+            "options": [
+              {
+                "value": "weibo",
+                "label": "微博",
+                "tip": "多次导入同一话题会导致数据重复，需要站长手动清理",
+                "setting": [ // 当选中微博时，应该展示的表单选项
+                  {
+                    "field_key": "topic",
+                    "field_name": "话题",
+                    "field_ext": {},
+                    "field_placeholder": "搜索想要导入站点的话题",
+                    "field_tip": "",
+                    "type": 0,
+                    "type_desc": "单行文本框"
+                  }
+                ]
+              },
+              {
+                "value": "tieba",
+                "label": "贴吧",
+                "tip": "多次导入同一话题会导致数据重复，需要站长手动清理",
+                "setting": [ // 当选中微博时，应该展示的表单选项
+                  {
+                    "field_key": "topic",
+                    "field_name": "话题",
+                    "field_ext": {},
+                    "field_placeholder": "搜索想要导入站点的话题",
+                    "field_tip": "",
+                    "type": 0,
+                    "type_desc": "单行文本框"
+                  }
+                ]
+              },
+              {
+                "value": "douban",
+                "label": "豆瓣",
+                "tip": "多次导入同一话题会导致数据重复，需要站长手动清理",
+                "setting": [ // 当选中微博时，应该展示的表单选项
+                  {
+                    "field_key": "topic",
+                    "field_name": "话题",
+                    "field_ext": {},
+                    "field_placeholder": "搜索想要导入站点的话题",
+                    "field_tip": "",
+                    "type": 0,
+                    "type_desc": "单行文本框"
+                  }
+                ]
+              },
+              {
+                "value": "wechat",
+                "label": "微信公众号",
+                "tip": "",
+                "setting": [ // 当选中微博时，应该展示的表单选项
+                  {
+                    "field_key": "link",
+                    "field_name": "链接",
+                    "field_ext": {},
+                    "field_placeholder": "输入想要导入的公众号文章链接",
+                    "field_tip": "",
+                    "type": 7,
+                    "type_desc": "可添加的单行文本框（文本框后面跟一个添加按钮，大于一个时，有可删除的删除）"
+                  }
+                ]
+              },
+              {
+                "value": "xingqiu",
+                "label": "知识星球",
+                "tip": "仅支持导入已加入的星球内容，多次导入同一话题会导致数据重复，需要站长手动清理",
+                "setting": [ // 当选中微博时，应该展示的表单选项
+                  {
+                    "field_key": "topic",
+                    "field_name": "话题",
+                    "field_ext": {},
+                    "field_placeholder": "搜索想要导入站点的话题",
+                    "field_tip": "",
+                    "type": 0,
+                    "type_desc": "单行文本框"
+                  }
+                ]
+              },
+            ]
+          }, // 字段扩展信息
+          "field_placeholder": "请选择要导入的平台",
+          "field_tip": "",
+          "type": 6,
+          "type_desc": "单选下拉框"
+        },
+        {
+          "field_key": "isUsed",
+          "field_name": "是否启用",
+          "field_ext": {}, // 字段扩展信息
+          "field_placeholder": "",
+          "field_tip": "",
+          "type": 8,
+          "type_desc": "开关"
+        },
+        {
+          "field_key": "init",
+          "field_name": "初始化数据",
+          "field_ext": {}, // 字段扩展信息
+          "field_placeholder": "",
+          "field_tip": "点击此按钮，会进行数据初始化操作",
+          "type": 9,
+          "type_desc": "按钮",
+          "event_opt": { // 事件选项，这个目前只有button类型的表单项才有这个，这个也主要是给后端初始化事情的使用，就是点击按钮直接请求一个接口即可，接口返回什么就toast提示什么
+            "name": "request",
+            "api": "/api/backAdmin/plugin/data.test", // 随便写的
+            "method": "post",
+            "params": {},
+            "setTimeout": 0 // 0 代表不轮询，1000 代表 1s 轮询一次
+          } 
+        },
+      ],
+      topicContent: '',
+      topicNum: '',
+      speed: false, // 进度显示
+      importing: '',
+      progress: 0,
+      startTiming: false,
+      contentSourceValue: 'weibo',
+      accuntlnroCookie: '',
+      accuntlnroToken: '',
+      planetCookie: '',
+      planetUserAgent: '',
+      totalNumber: 0,
+      contentSource: [
+        // {
+        //   name: '微博',
+        //   id: 1
+        // },
+        // {
+        //   name: '贴吧',
+        //   id: 2
+        // },
+        // {
+        //   name: '豆瓣',
+        //   id: 3
+        // },
+        // {
+        //   name: '微信公众号',
+        //   id: 4
+        // },
+        // {
+        //   name: '知识星球',
+        //   id: 5
+        // },
+      ],
+      contentSourceType: '',
+      contentSourceTip: '',
+      contentPlaceholder: '',
+      officialAccountLink: [
+        {
+          linkData: '',
+        }
+      ],
+      fileList: [],
+      enclosureList: [],
     }
   },
   created() {
     this.importDataBtn();
+    this.contentImportData();
   },
   methods:{
-    determineBtn(type, scope) {
-      this.uploadType = type;
-      this.uploadDetails = scope.row;
-      this.dialogVisible = true;
-    },
+    // determineBtn(type, scope) {
+    //   this.uploadType = type;
+    //   this.uploadDetails = scope.row;
+    //   this.dialogVisible = true;
+    // },
     // detailsClick() {
     //   this.dialogVisible = true;
     // },
+    contentImportData() {
+      this.settingData.forEach(item => {
+        if (item.type === 6) {
+          item.field_ext.options.forEach((items, index) => {
+            if (index === 0) {
+              this.contentSourceValue = items.value;
+              this.contentSourceType = items.setting[0].type;
+              this.contentSourceTip = items.tip;
+              this.contentPlaceholder = items.setting[0].field_placeholder
+            }
+            this.contentSource.push({
+              name: items.label,
+              value: items.value,
+              tip: items.tip,
+              type: items.setting[0].type,
+              placeholder: items.setting[0].field_placeholder,
+            })
+          })
+        }
+      })
+      console.log(this.contentSource);
+    },
     plugInRelease(id, num) {
       this.appFetch({
         url:'panel_operate_post_v3',
@@ -62,9 +314,6 @@ export default {
           this.importDataBtn();
         }
       })
-    },
-    handleChange(file, fileList) {
-      this.fileList = fileList.slice(-3);
     },
     beforePluginUpload(file) {
       const isLt20M = file.size / 1024 / 1024 < 20;
@@ -130,6 +379,156 @@ export default {
         default:
           break;
       }
+    },
+    contentSourceChange(value) {
+      this.contentSource.forEach(item => {
+        if (item.value === value) {
+          this.contentSourceType = item.type;
+          this.contentSourceTip = item.tip;
+          this.contentPlaceholder = item.placeholder;
+        }
+      })
+    },
+    increaseLink() {
+      if (this.officialAccountLink.length < 20) {
+        this.officialAccountLink.push(
+          {
+            linkData: '',
+          }
+        );
+      }
+    },
+    deleteLink(index) {
+      if (this.officialAccountLink.length > 1 ) {
+        this.officialAccountLink.splice(index, 1);
+      }
+    },
+    topicNumInput(value) {
+      if (Number(value) > 1000) {
+        this.$message.error('一次最多导入1000条');
+        this.topicNum = '';
+      }
+    },
+    importContentBtn() {
+      let params = {};
+      if (this.contentSourceValue === 'wechat') {
+        let officialAccountUrlArr = [];
+        this.officialAccountLink.map(item => {
+          officialAccountUrlArr.push(item.linkData);
+        })
+        params = {
+          officialAccountUrl: officialAccountUrlArr,
+          platform: this.contentSourceValue,
+        }
+      } else if (this.contentSourceValue === 'xingqiu') {
+        params = {
+          topic: this.topicContent,
+          cookie: this.planetCookie,
+          userAgent: this.planetUserAgent,
+          number: this.topicNum,
+          platform: this.contentSourceValue,
+        }
+      } else {
+        params = {
+          topic: this.topicContent,
+          number: this.topicNum,
+          platform: this.contentSourceValue,
+        }
+      }
+      this.appFetch({
+        url:'create_crawler_get',
+        method:'get',
+        data: params,
+      }).then(res => {
+        if (res.errors){
+          this.$message.error(res.errors[0].code);
+        } else {
+          if (res.Code !== 0) {
+            this.$message.error(res.Message);
+            return
+          }
+          this.speed = true;
+          clearInterval(this.timer);
+          this.startTiming = false;
+          this.progress = 0;
+          this.importing = 1;
+          this.timer = setInterval(this.crawlerQuery, 2000);
+          this.$message({
+            message: res.Message,
+            type: 'success'
+          });
+        }
+      })
+    },
+    crawlerQuery() {
+      this.appFetch({
+        url:'check_crawler_get',
+        method:'get',
+      }).then( res => {
+        if (res.errors){
+          clearInterval(this.timer);
+          this.$message.error(res.errors[0].code);
+        } else {
+          if (res.Code !== 0) {
+            clearInterval(this.timer);
+            this.$message.error(res.Message);
+            return
+          }
+          const crawlerData = res.Data;
+          if (crawlerData.startCrawlerTime !== 0) {
+            this.startTiming = true;
+            this.importing = crawlerData.status;
+            this.progress = crawlerData.progress;
+            // if (crawlerData.status === 2 && crawlerData.progress === 100) {
+            //   this.progress = 100;
+            //   clearInterval(this.timer);
+            // }
+            if (crawlerData.status !== 0 && crawlerData.status !== 1 && crawlerData.status !== 2) {
+              clearInterval(this.timer);
+            }
+          }
+          if (this.startTiming) {
+            this.importing = crawlerData.status;
+            this.progress = crawlerData.progress;
+            if (crawlerData.status === 2 && crawlerData.progress === 100) {
+              this.progress = 100;
+              this.totalDataNumber = crawlerData.totalDataNumber;
+              clearInterval(this.timer);
+            }
+            if (crawlerData.status !== 0 && crawlerData.status !== 1 && crawlerData.status !== 2) {
+              clearInterval(this.timer);
+            }
+          }
+        }
+      })
+    },
+    determineBtn() {
+      this.speed = false;
+      clearInterval(this.timer);
+    },
+    cancelBtn() {
+      history.go(0);
+    },
+    retryBtn() {
+      this.importContentBtn();
+    },
+    uploaderEnclosure(file) {
+      console.log(file);
+    },
+    uploaderRemove(file) {
+      console.log(file);
+    },
+    handlePreview(file) {
+      console.log(file);
+    },
+    uploaderImg(file) {
+      console.log(file);
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePreview(file) {
+      console.log(file);
     }
   },
   components:{
--- resources/frame/src/admin/view/site/global/pluginUnitView.vue
+++ resources/frame/src/admin/view/site/global/pluginUnitView.vue
@@ -48,53 +48,175 @@
         <el-table-column label="操作"  class="plugin-unit-post">
           <template slot-scope="scope">
             <div class="plugin-unit-post__box">
-              <!-- <span class="plugin-unit-post__box-text" @click="determineBtn('details', scope)">查看详情</span> -->
               <span class="plugin-unit-post__box-text" v-if="scope.row.status === 0" @click="plugInRelease(scope.row.app_id, 1)">发布</span>
               <span class="plugin-unit-post__box-text" v-if="scope.row.status === 1" @click="plugInRelease(scope.row.app_id, 2)">下线</span>
+              <span class="plugin-unit-post__box-text" @click="determineBtn('details', scope)">设置</span>
               <span class="plugin-unit-post__box-detal" @click="plugInRelease(scope.row.app_id, 3)">删除</span>
               <!-- <span class="plugin-unit-post__box-text">编辑</span> -->
             </div>
           </template>
         </el-table-column>
-        <!-- <el-dialog
-          title="提示"
+        <el-dialog
+          title="设置"
           :visible.sync="dialogVisible"
-          width="40%"
-          height="40%"
+          width="55%"
+          height="50%"
           :append-to-body="true"
           :lock-scroll="false"
           class="plugin-unit-post__box-dialog"
           >
-            <div class="plugin-unit-post__box-upload" v-if="uploadType ==='upload'">
-              <p class="plugin-unit-post__box-upload-file">
-                <span class="plugin-unit-post__box-upload-file__left">插件上传:</span>
-                <el-upload
-                  class="upload-demo"
-                  action
-                  :http-request="uploaderPlugin"
-                  :on-change="handleChange"
-                  :before-upload="beforePluginUpload"
-                  :file-list="fileList">
-                  <el-button size="small" type="primary">选择文件</el-button>
-                </el-upload>
-              </p>
-            </div>
-            <div class="plugin-unit-post__box-upload" v-if="uploadType ==='details'">
-              <div class="plugin-unit-post__box-upload-name">
-                <span class="plugin-unit-post__box-upload-name__left">插件名称:</span>
-                <p
-                  class="plugin-unit-post__box-upload-name__right"
-                  style="height: 36PX;width: 200PX"
-                >
-                 {{uploadDetails.name}}
+            <div class="plugin-unit-post__box-upload">
+              <div class="plugin-unit-post__box-upload-name" v-for="(item, index) in settingData" :key="index">
+                <p :class="item.type === 1 ? 'plugin-unit-post__box-upload-name__textarea' : 'plugin-unit-post__box-upload-name__left'">
+                  {{item.field_name}}
                 </p>
+                <div class="plugin-unit-post__box-upload-name__right">
+                  <el-input
+                    v-if="item.type === 0"
+                    class="elinput"
+                    size="small"
+                    maxlength="10"
+                    :placeholder="item.field_placeholder"
+                    @input="downloadsNumInput"
+                  ></el-input>
+
+                  <el-input
+                    v-if="item.type === 1"
+                    type="textarea"
+                    :rows="2"
+                    :placeholder="item.field_placeholder">
+                  </el-input>
+
+                  <el-radio-group v-model="radio" v-if="item.type === 2">
+                    <el-radio :label="3">备选项</el-radio>
+                    <el-radio :label="6">备选项</el-radio>
+                    <el-radio :label="9">备选项</el-radio>
+                  </el-radio-group>
+                  
+                  <el-checkbox-group v-model="checkList" v-if="item.type === 3">
+                    <el-checkbox label="复选框 A"></el-checkbox>
+                    <el-checkbox label="复选框 B"></el-checkbox>
+                    <el-checkbox label="复选框 C"></el-checkbox>
+                    <el-checkbox label="禁用"></el-checkbox>
+                    <el-checkbox label="选中且禁用"></el-checkbox>
+                  </el-checkbox-group>
+                  
+                  <div v-if="item.type === 4">
+                    <el-upload
+                      class="upload-demo"
+                      action
+                      :http-request="uploaderEnclosure"
+                      :on-preview="handlePreview"
+                      :on-remove="handleRemove"
+                      :file-list="fileList"
+                      list-type="picture">
+                      <el-button size="small" type="primary">图片上传</el-button>
+                    </el-upload>
+                  </div>
+                  
+                  <div v-if="item.type === 5">
+                    <el-upload
+                      class="upload-demo"
+                      action
+                      :http-request="uploaderImg"
+                      :on-preview="uploaderPreview"
+                      :on-remove="uploaderRemove"
+                      :file-list="enclosureList">
+                      <el-button size="small" type="primary">附件上传</el-button>
+                    </el-upload>
+                  </div>
+
+                  <div class="plugin-unit-post__box-upload-name__right-import" v-if="item.type === 6">
+                    <Card class="content-import-text">
+                      <div class="content-import-source">
+                        <!-- <span class="content-import-source__title">来源</span> -->
+                        <el-select class="content-import-source__option" v-model="contentSourceValue" placeholder="选择内容来源" clearable @change="contentSourceChange">
+                          <el-option class="content-import-source__option-selected" v-for="(item, index) in contentSource" :key="index" :label="item.name" :value="item.value"></el-option>
+                        </el-select>
+                      </div>
+                    </Card>
+
+                    <div v-if="contentSourceTip" class="content-import">{{contentSourceTip}}</div>
+                    <!-- <div v-if="contentSourceValue === 'xingqiu'" class="content-import">仅支持导入已加入的星球内容，多次导入同一话题会导致数据重复，需要站长手动清理</div> -->
+
+                    <Card v-if="contentSourceValue === 'xingqiu'">
+                      <div class="content-import-planet">
+                        <p class="content-import-planet__left">Cookie</p>
+                        <el-input v-model="planetCookie"></el-input>
+                        <a href="https://discuz.com/manual-admin/2.html#_2-10-2-知识星球cookie和user-agent" target="_blank" class="content-import-planet__right"><i class="el-icon-question"></i></a>
+                      </div>
+                    </Card>
+                    
+                    <Card v-if="contentSourceValue === 'xingqiu'">
+                      <div class="content-import-planet">
+                        <p class="content-import-planet__left">User-Agent</p>
+                        <el-input v-model="planetUserAgent"></el-input>
+                        <a href="https://discuz.com/manual-admin/2.html#_2-10-2-知识星球cookie和user-agent" target="_blank" class="content-import-planet__right"><i class="el-icon-question"></i></a>
+                      </div>
+                    </Card>
+
+                    <Card v-if="contentSourceType === 0">
+                      <div class="content-import-search">
+                        <el-input v-model="topicContent" placeholder='搜索想要导入站点的话题'></el-input>
+                      </div>
+                    </Card>
+
+                    <Card  v-if="contentSourceType === 7">
+                      <div class="content-import-official" v-for="(item, index) in officialAccountLink" :key="index">
+                        <el-input v-model="item.linkData" placeholder="输入想要导入公众号文章链接"></el-input>
+                        <div class="content-import-official__btn">
+                          <span class="content-import-official__btn-iconleft" v-if="index === officialAccountLink.length - 1" @click="increaseLink">
+                            <i class="el-icon-circle-plus-outline"></i>
+                          </span>
+                          <span class="content-import-official__btn-iconright" v-if="officialAccountLink.length !== 1" @click="deleteLink(index)">
+                            <i class="el-icon-remove-outline"></i>
+                          </span>
+                        </div>
+                      </div>
+                    </Card>
+
+                    <Card class="content-import-layer" v-if="contentSourceValue !== 'wechat'">
+                      <CardRow description="一次最多导入1000条">
+                        <div class="content-import-num">
+                          <p class="content-import-num__topic">导入条数</p>
+                          <el-input type="number" v-model="topicNum" @input="topicNumInput"></el-input>
+                        </div>
+                      </CardRow>
+                    </Card>
+
+                    <!-- <Card class="footer-btn" >
+                      <el-button type="primary" size="medium" @click="importContentBtn">提交</el-button>
+                    </Card> -->
+
+                    <Card v-if="speed">
+                      <el-progress :percentage="progress" class="progress-box"></el-progress>
+                      <p class="progress-box__importing" v-if="importing === 1">正在导入中</p>
+                      <div class="progress-box__success" v-if="importing === 2">
+                        <p class="progress-box__success-confirm">成功导入{{totalDataNumber}}条数据，稍后可在前台查看</p>
+                        <el-button type="primary" size="medium" @click="determineBtn">确认</el-button>
+                      </div>
+                      <div class="progress-box__fail" v-if="importing !== 0 && importing !== 1 && importing !== 2">
+                        <p class="progress-box__fail-fotter">导入失败</p>
+                        <el-button type="primary" size="medium" @click="retryBtn">重试</el-button>
+                        <el-button size="medium" @click="cancelBtn">取消</el-button>
+                      </div>
+                    </Card>
+                  </div>
+
+                  <el-switch
+                    v-if="item.type === 8"
+                    active-color="#336699"
+                    inactive-color="#bbbbbb"
+                  >
+                  </el-switch>
+                </div>
               </div>
             </div>
             <span slot="footer" class="dialog-footer">
               <el-button @click="dialogVisible = false">取 消</el-button>
               <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
             </span>
-        </el-dialog> -->
+        </el-dialog>
       </el-table>
     </Card>
   </div>
